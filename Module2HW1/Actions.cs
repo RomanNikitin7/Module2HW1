39e3bb65c1ecaed46ca4043ed2275151ec011ff0
Roman Nikitin <roman.nikitin@internera.systems>
2024-03-17 22:59:15 +0200

    commit

---
+++ Module2HW1/Actions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logger
+{
+    internal class Actions
+    {
+        public Result Method1()
+        {
+            Logger.Instance.Log(LogType.Info, "Start method: Method1");
+            return new Result() { Status = true };
+        }
+        public Result Method2()
+        {
+            Logger.Instance.Log(LogType.Warning, "Skipped logic in method: Method2");
+            return new Result() { Status = true };
+        }
+        public Result Method3()
+        {
+            return new Result()
+            {
+                Status = false,
+                ErrorMessage = "I broke a logic"
+            };
+        }
+    }
+}
---
+++ Module2HW1/Starter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logger
+{
+    internal class Starter
+    {
+        public void Run()
+        {
+            var random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var actions = new Actions();
+                Result result = null;
+                switch (random.Next(1, 4))
+                {
+                    case 1:
+                        result = actions.Method1();
+                        break;
+                    case 2:
+                        result = actions.Method2();
+                        break;  
+                    case 3:
+                        result = actions.Method3();
+                        break;
+                }
+
+                if (result != null && !result.Status)
+                {
+                    Logger.Instance.Log(LogType.Error, "Action failed by a reason:" + result.ErrorMessage);
+                }
+            }
+
+            File.WriteAllText("log.txt", Logger.Instance.GetLogString());
+        }
+    }
+}
